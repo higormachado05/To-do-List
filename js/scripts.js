5d0c8131afce5f6c4d2f6ef3bab53a50d5aa5728
Higor Machado <machado.higor98@gmail.com>
2023-06-30 00:42:30 -0300

    Eventos todo JS

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,10 @@ const todoForm = document.querySelector("#todo-form");
 const todoInput = document.querySelector("#todo-input");
 const todoList = document.querySelector("#todo-list");
 const editForm = document.querySelector("#edit-form");
-const teditInput = document.querySelector("#edit-input");
+const editInput = document.querySelector("#edit-input");
 const cancelEditBtn = document.querySelector("#cancel-edit-btn");
 
+let oldInputValue;
 
 //Funções
 const saveTodo = (text) => {
@@ -37,6 +38,25 @@ const saveTodo = (text) => {
   todoInput.focus();
 }
 
+const toggleForms = () =>{
+  editForm.classList.toggle("hide");
+  todoForm.classList.toggle("hide");
+  todoList.classList.toggle("hide");
+}
+
+const updateTodo = (text) =>{
+
+  const todos = document.querySelectorAll(".todo");
+
+  todos.forEach((todo) => {
+    let todoTitle = todo.querySelector("h3");
+
+    if(todoTitle.innerText === oldInputValue){
+      todoTitle.innerText = text;
+    }
+  })
+}
+
 //Eventos
 todoForm.addEventListener("submit", (e) =>{
   e.preventDefault();
@@ -44,7 +64,49 @@ todoForm.addEventListener("submit", (e) =>{
   const inputValue = todoInput.value;
 
   if(inputValue){
-   saveTodo(inputValue)
+   saveTodo(inputValue);
   }
 });
 
+document.addEventListener("click", (e) => {
+  const targetEl = e.target
+  const parentEl = targetEl.closest("div");
+  let todoTitle;
+
+  if (parentEl && parentEl.querySelector("h3")){
+    todoTitle = parentEl.querySelector("h3").innerText;
+  }
+
+  if (targetEl.classList.contains("finish-todo")){
+    parentEl.classList.toggle("done");
+  }
+
+  if (targetEl.classList.contains("remove-todo")){
+    parentEl.remove();
+  }
+
+  if (targetEl.classList.contains("edit-todo")){
+    toggleForms();
+
+    editInput.value = todoTitle;
+    oldInputValue = todoTitle;
+  }
+});
+
+cancelEditBtn.addEventListener("click", (e) =>{
+  e.preventDefault();
+
+  toggleForms();
+});
+
+editForm.addEventListener("submit", (e) =>{
+  e.preventDefault();
+
+  const editInputValue = editInput.value;
+
+  if(editInputValue){
+    updateTodo(editInputValue)
+  }
+
+  toggleForms()
+});
